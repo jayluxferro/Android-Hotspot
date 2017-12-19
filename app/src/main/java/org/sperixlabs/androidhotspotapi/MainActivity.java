fc4141ed12493a02969aa3dd86bb134cf3e2161e
jayluxferro <jayluxferro@sperixlabs.org>
2017-12-19 10:48:15 +0000

    added tab

--- app/src/main/java/org/sperixlabs/androidhotspotapi/MainActivity.java
+++ app/src/main/java/org/sperixlabs/androidhotspotapi/MainActivity.java
@@ -3,6 +3,11 @@ package org.sperixlabs.androidhotspotapi;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,10 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*
+        FloatingActionButton fab =  findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,8 +32,64 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
+
+        //adding viewpager
+        ViewPager viewPager =  findViewById(R.id.pager);
+        // Assign created adapter to viewPager
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+
+        //adding tabLayout
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        // This method setup all required method for TabLayout with Viewpager
+        tabLayout.setupWithViewPager(viewPager);
     }
 
+    // implementing tabAdapter
+    public static class PagerAdapter extends FragmentPagerAdapter {
+        // As we are implementing two tabs
+        private static final int NUM_ITEMS = 3;
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        // For each tab different fragment is returned
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new Main();
+                case 1:
+                    return new Clients();
+                case 2:
+                    return new Config();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position == 0){
+                return "Main";
+            }else if(position == 1){
+                return "Clients";
+            }else if(position == 2){
+                return "Settings";
+            }else {
+                return "Tab " + (position + 1);
+            }
+        }
+    }
+
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -49,4 +111,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
